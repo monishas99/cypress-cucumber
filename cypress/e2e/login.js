608268584bb61121cae6b35a575dfd330f9453ae
Monisha Suresh <monishasuresh@monishas-air.lan>
2024-10-08 21:36:12 +1100

    first commit

---
+++ cypress/e2e/login.js
@@ -0,0 +1,18 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('I am on the Sauce Demo login page', () => {
+  cy.visit('https://www.saucedemo.com');
+});
+
+When('I enter valid username and password', () => {
+  cy.get('[data-test="username"]').type('standard_user');
+  cy.get('[data-test="password"]').type('secret_sauce');
+});
+
+When('I click the login button', () => {
+  cy.get('[data-test="login-button"]').click();
+});
+
+Then('I should be logged in successfully', () => {
+  cy.url().should('include', '/inventory.html');
+});
